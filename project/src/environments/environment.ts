e841f660d84ba7dd662096c76b7769e13814f690
Marshal Guo <92243872+MagneticGlitter@users.noreply.github.com>
2023-11-04 16:09:09 -0400

    Revert "Marshal"

--- project/src/environments/environment.ts
+++ project/src/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
-    production: false,
-    assemblyAIKey: '650497f71e3a40ae8f1721d0d78b2840', // Replace 'YOUR_API_KEY' with your actual AssemblyAI API key
-  };
     production: true,
     firebase: {
         apiKey: "",
